eddddc7b82f6d99d14e1fc7f6917cbbf4327948d
hllojune <gy12wns200@gmail.com>
2025-04-02 18:44:12 +0900

    3주차 실습과제 First Commit

---
+++ Ch05_CheckProgrammingLan/app/src/main/java/com/example/ch05_checkprogramminglan/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ch05_checkprogramminglan;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("20210843 박효준");
+    }
+}

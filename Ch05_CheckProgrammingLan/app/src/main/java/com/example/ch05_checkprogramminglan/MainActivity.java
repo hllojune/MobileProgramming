2dd40f33b9053a6717672496c61ddeb05b8f0c03
hllojune <gy12wns200@gmail.com>
2025-04-04 03:18:57 +0900

    [3주차 실습과제] xml코드 및 java코드 최종 수정

--- Ch05_CheckProgrammingLan/app/src/main/java/com/example/ch05_checkprogramminglan/MainActivity.java
+++ Ch05_CheckProgrammingLan/app/src/main/java/com/example/ch05_checkprogramminglan/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.ch05_checkprogramminglan;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox pythonCheckBox, jsCheckBox, goCheckBox, rustCheckBox, kotlinCheckBox;
+    private TextView showResult;
+    private Button doneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,38 +21,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("20210843 박효준");
 
-        CheckBox ch1 = (CheckBox)findViewById(R.id.Python);
-        CheckBox ch2 = (CheckBox)findViewById(R.id.JavaScript);
-        CheckBox ch3 = (CheckBox)findViewById(R.id.Go);
-        CheckBox ch4 = (CheckBox)findViewById(R.id.Rust);
-        CheckBox ch5 = (CheckBox)findViewById(R.id.Kotlin);
-        Button btn1 = (Button)findViewById(R.id.DoneButton);
-        Button btn2 = (Button)findViewById(R.id.cancelButton);
-        TextView tv = (TextView)findViewById(R.id.showResult);
+        // 체크박스 및 버튼 초기화
+        pythonCheckBox = findViewById(R.id.Python);
+        jsCheckBox = findViewById(R.id.JavaScript);
+        goCheckBox = findViewById(R.id.Go);
+        rustCheckBox = findViewById(R.id.Rust);
+        kotlinCheckBox = findViewById(R.id.Kotlin);
+        showResult = findViewById(R.id.showResult);
+        doneButton = findViewById(R.id.Done);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        // Done 버튼 클릭 이벤트 설정
+        doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckboxClicked(View view) {
-                boolean checked = ((CheckBox) view).isChecked();
-                String result = "";
-
-                switch (view.getId()) {
-                    case R.id.Python:
-                        if (checked) {
-                            result += ch1.getText() + " ";
-                            tv.setText(result);
-                        }
-                        else tv.setText(0);
-                        break;
-                }
-
-//                if (ch1.isChecked()) result += ch1.getText() + " ";
-//                if (ch2.isChecked()) result += ch2.getText() + " ";
-//                if (ch3.isChecked()) result += ch3.getText() + " ";
-//                if (ch4.isChecked()) result += ch4.getText() + " ";
-//                if (ch5.isChecked()) result += ch5.getText() + " ";
-//                tv.setText(result);
+            public void onClick(View v) {
+                showSelectedLanguages();
             }
         });
+    }
+
+    private void showSelectedLanguages() {
+        ArrayList<String> selectedLanguages = new ArrayList<>();
+
+        if (pythonCheckBox.isChecked()) selectedLanguages.add("Python");
+        if (jsCheckBox.isChecked()) selectedLanguages.add("JavaScript");
+        if (goCheckBox.isChecked()) selectedLanguages.add("Go");
+        if (rustCheckBox.isChecked()) selectedLanguages.add("Rust");
+        if (kotlinCheckBox.isChecked()) selectedLanguages.add("Kotlin");
+
+        if (selectedLanguages.isEmpty()) {
+            showResult.setText("No language selected.");
+        } else {
+            showResult.setText(String.join(" ", selectedLanguages));
+        }
     }
 }
